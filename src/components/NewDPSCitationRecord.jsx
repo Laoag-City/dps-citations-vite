ef7d6baca0e3fac48afd6d001d2c46c9a77958ff
Richard Alhama <richard.alhama@gmail.com>
2024-06-27 07:32:10 +0000

    violations list ok

--- src/components/NewDPSCitationRecord.jsx
+++ src/components/NewDPSCitationRecord.jsx
@@ -6,8 +6,63 @@ import axios from 'axios';
 import TopBar from './TopBar';
 import Footer from './Footer';
 
+const violationsList = [
+  { violation: 'arrogant driver', amount: 150 },
+  { violation: 'jaywalking', amount: 150 },
+  { violation: 'invalid/fake registration or unregistered', amount: 3000 },
+  { violation: 'improper display of licence plates', amount: 150 },
+  { violation: 'overspeeding', amount: 150 },
+  { violation: 'overloading', amount: 150 },
+  { violation: 'driving under influence of liquor', amount: 5000 },
+  { violation: 'defective accessories (side mirror, tail and head lights)', amount: 150 },
+  { violation: 'anti-nuisance', amount: 150 },
+  { violation: 'failure to wear helmet (driver)', amount: 1000 },
+  { violation: 'failure to wear helmet (backride)', amount: 1000 },
+  { violation: 'inappropriate use of helmet', amount: 1000 },
+  { violation: 'using mobile phone/ any similar device/ gadgets while driving or operating motorized vehicle', amount: 150 },
+  { violation: 'allowing children of any age to sit on the gas tank of any motorcycle while in motion', amount: 150 },
+  { violation: 'failure of driver of a 4-wheel vehicle including the front seat passenger to wear seatbelts', amount: 150 },
+  { violation: 'driving without driver\'s license', amount: 3000 },
+  { violation: 'driving with expired driver\'s license', amount: 3000 },
+  { violation: 'failure to carry driver\'s license while driving or operating a motorized vehicle', amount: 3000 },
+  { violation: 'failure to present driver\'s license', amount: 3000 },
+  { violation: 'driving with student permit', amount: 150 },
+  { violation: 'blocking pedestrian lane', amount: 150 },
+  { violation: 'no parking', amount: 150 },
+  { violation: 'no loading/unloading zone', amount: 150 },
+  { violation: 'wrong parking', amount: 150 },
+  { violation: 'double parking', amount: 150 },
+  { violation: 'obstruction', amount: 150 },
+  { violation: 'six meters', amount: 150 },
+  { violation: 'no trade name disregarding traffic sign/signal', amount: 150 },
+  { violation: 'no u-turn', amount: 150 },
+  { violation: 'no entry', amount: 150 },
+  { violation: 'no left turn', amount: 150 },
+  { violation: 'no right turn', amount: 150 },
+  { violation: 'driving on the wrong side', amount: 150 },
+  { violation: 'overspeeding', amount: 150 },
+  { violation: 'overcharging', amount: 1500 },
+  { violation: 'failure to display fare guide', amount: 150 },
+  { violation: 'failure to register at night', amount: 150 },
+  { violation: 'failure to carry mtop', amount: 150 },
+  { violation: 'failure to display "not for hire"', amount: 150 },
+  { violation: 'driving without tdic', amount: 150 },
+  { violation: 'expired tricycle driver i.d', amount: 250 },
+  { violation: 'plying national road', amount: 150 },
+  { violation: 'no trashcan', amount: 150 },
+  { violation: 'wearing slippers/sandals', amount: 150 },
+  { violation: 'using bonnet or mask', amount: 150 },
+  { violation: 'backriding', amount: 150 },
+  { violation: 'overloading', amount: 150 },
+  { violation: 'refuse conveyance', amount: 1000 },
+  { violation: 'using in the commission of a crime', amount: 5000 },
+  { violation: 'no fare guide', amount: 1500 },
+  { violation: 'speed limit of 50 kph passing through gilbert bridge', amount: 200 },
+  { violation: 'no loading or unloading along the southern and northern approach of the gilbert bridge', amount: 1000 }
+];
+
 function DPSCitationRecordForm() {
-  const currentTime = new Date().toISOString().slice(0, 16); // ISO string for datetime-local inpu
+  const currentTime = new Date().toISOString().slice(0, 16); // ISO string for datetime-local input
 
   const [formData, setFormData] = useState({
     firstName: '',
@@ -52,7 +107,12 @@ function DPSCitationRecordForm() {
     const { name, value } = event.target;
     const updatedViolations = formData.violations.map((violation, vIndex) => {
       if (index === vIndex) {
-        return { ...violation, [name]: value };
+        const selectedViolation = violationsList.find(v => v.violation === value);
+        return {
+          ...violation,
+          [name]: value,
+          amount: selectedViolation ? selectedViolation.amount : ''
+        };
       }
       return violation;
     });
@@ -79,8 +139,6 @@ function DPSCitationRecordForm() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     console.log(formData);
-    console.log(JSON.stringify(formData));
-    console.log(token);
     try {
       const response = await axios.post('https://apps.laoagcity.gov.ph:3002/dpscitations', JSON.stringify(formData), config);
       console.log("Response data: ", response.data);
@@ -142,7 +200,7 @@ function DPSCitationRecordForm() {
           <Col>
             <Form.Group controlId="timeApprehended">
               <Form.Label>Time Apprehended</Form.Label>
-              <Form.Control type="time" name="timeApprehended" value={formData.timeApprehended} onChange={handleChange} />
+              <Form.Control type="datetime-local" name="timeApprehended" value={formData.timeApprehended} onChange={handleChange} />
             </Form.Group>
           </Col>
         </Row>
@@ -174,32 +232,24 @@ function DPSCitationRecordForm() {
             </Form.Group>
           </Col>
         </Row>
-        <Row>
-          <Col>
-            <Form.Group controlId="amendStatus">
-              <Form.Check type="checkbox" label="Amend Status" name="amendStatus" checked={formData.amendStatus} onChange={handleChange} />
-            </Form.Group>
-          </Col>
-          <Col>
-            <Form.Group controlId="dateAmended">
-              <Form.Label>Date Amended</Form.Label>
-              <Form.Control type="datetime-local" name="dateAmended" value={formData.dateAmended} onChange={handleChange} disabled={!formData.amendStatus} />
-            </Form.Group>
-          </Col>
-        </Row>
         <h5>Violations</h5>
         {formData.violations.map((violation, index) => (
           <Row key={index}>
             <Col>
               <Form.Group controlId={`violation-${index}`}>
                 <Form.Label>Violation</Form.Label>
-                <Form.Control type="text" name="violation" value={violation.violation} onChange={(e) => handleViolationChange(index, e)} required />
+                <Form.Control as="select" name="violation" value={violation.violation} onChange={(e) => handleViolationChange(index, e)}>
+                  <option value="">Select violation</option>
+                  {violationsList.map((v, i) => (
+                    <option key={i} value={v.violation}>{v.violation}</option>
+                  ))}
+                </Form.Control>
               </Form.Group>
             </Col>
             <Col>
               <Form.Group controlId={`amount-${index}`}>
                 <Form.Label>Amount</Form.Label>
-                <Form.Control type="number" name="amount" value={violation.amount} onChange={(e) => handleViolationChange(index, e)} required />
+                <Form.Control type="number" name="amount" value={violation.amount} onChange={(e) => handleViolationChange(index, e)} readOnly />
               </Form.Group>
             </Col>
             <Col>
@@ -216,7 +266,9 @@ function DPSCitationRecordForm() {
         <Button variant="warning" onClick={addViolation}>Add Violation</Button>
         <br />
         <Button type="submit">Submit</Button>
-        <Button variant="secondary" onClick={backToHome}>Back to Home</Button>
+        <Button variant="secondary" onClick={backToHome}>
+          Back to Home
+        </Button>{' '}
       </Form>
       <Footer />
     </Container>
