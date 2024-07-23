77b0b46c03a5027cb7388c7017b5f67918276e28
Richard Alhama <richard.alhama@gmail.com>
2024-07-23 03:52:13 +0000

    working apprehender dropdown

--- src/components/NewDPSCitationRecord.jsx
+++ src/components/NewDPSCitationRecord.jsx
@@ -7,62 +7,63 @@ import TopBar from './TopBar';
 import Footer from './Footer';
 
 /* const violationsList = [
-  { violation: 'arrogant driver', amount: 150 },
-  { violation: 'jaywalking', amount: 150 },
-  { violation: 'invalid/fake registration or unregistered', amount: 3000 },
-  { violation: 'improper display of licence plates', amount: 150 },
-  { violation: 'overspeeding', amount: 150 },
-  { violation: 'overloading', amount: 150 },
-  { violation: 'driving under influence of liquor', amount: 5000 },
-  { violation: 'defective accessories (side mirror, tail and head lights)', amount: 150 },
-  { violation: 'anti-nuisance', amount: 150 },
-  { violation: 'failure to wear helmet (driver)', amount: 1000 },
-  { violation: 'failure to wear helmet (backride)', amount: 1000 },
-  { violation: 'inappropriate use of helmet', amount: 1000 },
-  { violation: 'using mobile phone/ any similar device/ gadgets while driving or operating motorized vehicle', amount: 150 },
-  { violation: 'allowing children of any age to sit on the gas tank of any motorcycle while in motion', amount: 150 },
-  { violation: 'failure of driver of a 4-wheel vehicle including the front seat passenger to wear seatbelts', amount: 150 },
-  { violation: 'driving without driver\'s license', amount: 3000 },
-  { violation: 'driving with expired driver\'s license', amount: 3000 },
-  { violation: 'failure to carry driver\'s license while driving or operating a motorized vehicle', amount: 3000 },
-  { violation: 'failure to present driver\'s license', amount: 3000 },
-  { violation: 'driving with student permit', amount: 150 },
-  { violation: 'blocking pedestrian lane', amount: 150 },
-  { violation: 'no parking', amount: 150 },
-  { violation: 'no loading/unloading zone', amount: 150 },
-  { violation: 'wrong parking', amount: 150 },
-  { violation: 'double parking', amount: 150 },
-  { violation: 'obstruction', amount: 150 },
-  { violation: 'six meters', amount: 150 },
-  { violation: 'no trade name disregarding traffic sign/signal', amount: 150 },
-  { violation: 'no u-turn', amount: 150 },
-  { violation: 'no entry', amount: 150 },
-  { violation: 'no left turn', amount: 150 },
-  { violation: 'no right turn', amount: 150 },
-  { violation: 'driving on the wrong side', amount: 150 },
-  { violation: 'overspeeding', amount: 150 },
-  { violation: 'overcharging', amount: 1500 },
-  { violation: 'failure to display fare guide', amount: 150 },
-  { violation: 'failure to register at night', amount: 150 },
-  { violation: 'failure to carry mtop', amount: 150 },
-  { violation: 'failure to display "not for hire"', amount: 150 },
-  { violation: 'driving without tdic', amount: 150 },
-  { violation: 'expired tricycle driver i.d', amount: 250 },
-  { violation: 'plying national road', amount: 150 },
-  { violation: 'no trashcan', amount: 150 },
-  { violation: 'wearing slippers/sandals', amount: 150 },
-  { violation: 'using bonnet or mask', amount: 150 },
-  { violation: 'backriding', amount: 150 },
-  { violation: 'overloading', amount: 150 },
-  { violation: 'refuse conveyance', amount: 1000 },
-  { violation: 'using in the commission of a crime', amount: 5000 },
-  { violation: 'no fare guide', amount: 1500 },
-  { violation: 'speed limit of 50 kph passing through gilbert bridge', amount: 200 },
-  { violation: 'no loading or unloading along the southern and northern approach of the gilbert bridge', amount: 1000 }
+{ violation: 'arrogant driver', amount: 150 },
+{ violation: 'jaywalking', amount: 150 },
+{ violation: 'invalid/fake registration or unregistered', amount: 3000 },
+{ violation: 'improper display of licence plates', amount: 150 },
+{ violation: 'overspeeding', amount: 150 },
+{ violation: 'overloading', amount: 150 },
+{ violation: 'driving under influence of liquor', amount: 5000 },
+{ violation: 'defective accessories (side mirror, tail and head lights)', amount: 150 },
+{ violation: 'anti-nuisance', amount: 150 },
+{ violation: 'failure to wear helmet (driver)', amount: 1000 },
+{ violation: 'failure to wear helmet (backride)', amount: 1000 },
+{ violation: 'inappropriate use of helmet', amount: 1000 },
+{ violation: 'using mobile phone/ any similar device/ gadgets while driving or operating motorized vehicle', amount: 150 },
+{ violation: 'allowing children of any age to sit on the gas tank of any motorcycle while in motion', amount: 150 },
+{ violation: 'failure of driver of a 4-wheel vehicle including the front seat passenger to wear seatbelts', amount: 150 },
+{ violation: 'driving without driver\'s license', amount: 3000 },
+{ violation: 'driving with expired driver\'s license', amount: 3000 },
+{ violation: 'failure to carry driver\'s license while driving or operating a motorized vehicle', amount: 3000 },
+{ violation: 'failure to present driver\'s license', amount: 3000 },
+{ violation: 'driving with student permit', amount: 150 },
+{ violation: 'blocking pedestrian lane', amount: 150 },
+{ violation: 'no parking', amount: 150 },
+{ violation: 'no loading/unloading zone', amount: 150 },
+{ violation: 'wrong parking', amount: 150 },
+{ violation: 'double parking', amount: 150 },
+{ violation: 'obstruction', amount: 150 },
+{ violation: 'six meters', amount: 150 },
+{ violation: 'no trade name disregarding traffic sign/signal', amount: 150 },
+{ violation: 'no u-turn', amount: 150 },
+{ violation: 'no entry', amount: 150 },
+{ violation: 'no left turn', amount: 150 },
+{ violation: 'no right turn', amount: 150 },
+{ violation: 'driving on the wrong side', amount: 150 },
+{ violation: 'overspeeding', amount: 150 },
+{ violation: 'overcharging', amount: 1500 },
+{ violation: 'failure to display fare guide', amount: 150 },
+{ violation: 'failure to register at night', amount: 150 },
+{ violation: 'failure to carry mtop', amount: 150 },
+{ violation: 'failure to display "not for hire"', amount: 150 },
+{ violation: 'driving without tdic', amount: 150 },
+{ violation: 'expired tricycle driver i.d', amount: 250 },
+{ violation: 'plying national road', amount: 150 },
+{ violation: 'no trashcan', amount: 150 },
+{ violation: 'wearing slippers/sandals', amount: 150 },
+{ violation: 'using bonnet or mask', amount: 150 },
+{ violation: 'backriding', amount: 150 },
+{ violation: 'overloading', amount: 150 },
+{ violation: 'refuse conveyance', amount: 1000 },
+{ violation: 'using in the commission of a crime', amount: 5000 },
+{ violation: 'no fare guide', amount: 1500 },
+{ violation: 'speed limit of 50 kph passing through gilbert bridge', amount: 200 },
+{ violation: 'no loading or unloading along the southern and northern approach of the gilbert bridge', amount: 1000 }
 ];
- */
+*/
 function DPSCitationRecordForm() {
-  const [violationsList, setViolationsList] = useState(null);
+  const [violationsList, setViolationsList] = useState([]);
+  const [apprehendersList, setApprehendersList] = useState([]);
   const [error, setError] = useState('');
   const currentTime = new Date().toISOString().slice(0, 16); // ISO string for datetime-local input
 
@@ -83,6 +84,23 @@ function DPSCitationRecordForm() {
       }
     };
 
+    const fetchApprehendersData = async () => {
+      try {
+        const response = await axios.get('https://apps.laoagcity.gov.ph:3002/apprehenders', {
+        });
+        setApprehendersList(response.data);
+        // Log fetched data
+        //console.log("Fetched apprehendersList: ", response.data); 
+      } catch (error) {
+        if (error.response && error.response.status === 401) {
+          setError('Unauthorized access please authenticate')
+        } else {
+          setError('Failed to fetch data. Please try again later.');
+        }
+      }
+    };
+
+    fetchApprehendersData();
     fetchViolationsData();
   }, []);
 
@@ -261,7 +279,12 @@ function DPSCitationRecordForm() {
           <Col>
             <Form.Group controlId="apprehendingOfficer">
               <Form.Label>Apprehending Officer</Form.Label>
-              <Form.Control type="text" name="apprehendingOfficer" value={formData.apprehendingOfficer} onChange={handleChange} />
+              <Form.Control as="select" name="apprehendingOfficer" value={formData.apprehendingOfficer} onChange={handleChange}>
+                <option value="">Select apprehending officer</option>
+                {apprehendersList.map((officer, index) => (
+                  <option key={index} value={officer.name}>{officer.name}</option>
+                ))}
+              </Form.Control>
             </Form.Group>
           </Col>
         </Row>
@@ -273,8 +296,8 @@ function DPSCitationRecordForm() {
                 <Form.Label>Violation</Form.Label>
                 <Form.Control as="select" name="violation" value={violation.violation} onChange={(e) => handleViolationChange(index, e)}>
                   <option value="">Select violation</option>
-                  {violationsList.map((v, i) => (
-                    <option key={i} value={v.violation}>{v.violation}</option>
+                  {violationsList.map((violation, index) => (
+                    <option key={index} value={violation.violation}>{violation.violation}</option>
                   ))}
                 </Form.Control>
               </Form.Group>
