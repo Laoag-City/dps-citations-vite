dee375cd0133f1c5063c42cbd8020f5d9c7e260d
Richard Alhama <richard.alhama@gmail.com>
2024-07-31 05:29:48 +0000

    removed print

--- src/components/Dashboard/CitationTable.jsx
+++ src/components/Dashboard/CitationTable.jsx
@@ -1,93 +1,84 @@
 import PropTypes from 'prop-types';
-import { useState, useRef } from 'react';
+import { useState } from 'react';
 import { useSelector } from 'react-redux';
+
 import { Table, Button } from 'react-bootstrap';
 import { formatDate } from '../../utils/dateUtils';
 import { sumAmounts, getRowClass } from '../../utils/citationUtils';
 import { Link, useNavigate } from 'react-router-dom';
-import { useReactToPrint } from 'react-to-print';
-import './CitationTable.css'; // Import the CSS file for print styles
 
 const CitationTable = ({ citations, isPaidTab = false }) => {
+  // /console.log(userRole);
   const [sortField, setSortField] = useState('dateApprehended');
-  const [sortOrder, setSortOrder] = useState('asc');
+  const [sortOrder, setSortOrder] = useState('asc')
   const navigate = useNavigate();
   const { user } = useSelector((state) => state.auth);
-  const tableRef = useRef();
 
   const handleCommuteClick = (citation) => {
     navigate(`/commute-update/${citation._id}`);
+    //console.log('Commute action for citation:', citation);
   };
 
   const handlePaymentClick = (citation) => {
     navigate(`/payment-update/${citation._id}`);
   };
-
   const handleSortChange = (field) => {
     const order = (sortField === field && sortOrder === 'asc') ? 'desc' : 'asc';
     setSortField(field);
     setSortOrder(order);
   };
 
-  const handlePrint = useReactToPrint({
-    content: () => tableRef.current,
-    documentTitle: 'Citations',
-    onAfterPrint: () => console.log('Print successful!')
-  });
-
   return (
-    <>
-      <Table striped bordered hover ref={tableRef}>
-        <thead>
-          <tr>
-            <th onClick={() => handleSortChange('ticketNumber')}>Ticket Number</th>
-            <th onClick={() => handleSortChange('licenseNumber')}>License Number</th>
-            <th onClick={() => handleSortChange('dateApprehended')}>Date Apprehended</th>
-            <th onClick={() => handleSortChange('streetApprehended')}>Street Apprehended</th>
-            <th onClick={() => handleSortChange('plateNumber')}>Plate Number</th>
-            <th onClick={() => handleSortChange('vehicleColor')}>Vehicle Color</th>
-            <th onClick={() => handleSortChange('apprehendingOfficer')}>Apprehending Officer</th>
-            <th>Amount/Paid</th>
-            {!isPaidTab && <th onClick={() => handleSortChange('commuteStatus')}>Commuted</th>}
-            {!isPaidTab && <th>Paid</th>}
-            <th>Print</th>
-          </tr>
-        </thead>
-        <tbody>
-          {citations.length > 0 ? (
-            citations.map((citation) => (
-              <tr key={citation._id} className={getRowClass(citation.dateApprehended)}>
-                <td>{citation.ticketNumber}</td>
-                <td>{citation.licenseNumber}</td>
-                <td>{formatDate(citation.dateApprehended)}</td>
-                <td>{citation.streetApprehended}</td>
-                <td>{citation.plateNumber}</td>
-                <td>{citation.vehicleColor}</td>
-                <td>{citation.apprehendingOfficer}</td>
-                <td>{sumAmounts(citation.violations) + " : " + citation.amountPaid}</td>
-                {!isPaidTab && (
-                  <td>
-                    {user.userrole === 'dpshead' && (!citation.commuteStatus || citation.commuteStatus === undefined) ? <Button variant="warning" onClick={() => handleCommuteClick(citation)}>Commute</Button> : 'No'}
-                  </td>
-                )}
-                {!isPaidTab && (
-                  <td>
-                    {user.userrole === 'dpsstaff' && (!citation.paymentStatus || citation.paymentStatus === undefined) ? <Button variant="warning" onClick={() => handlePaymentClick(citation)}>Pay</Button> : 'No'}
-                  </td>
-                )}
-                <td><Link onClick={handlePrint}>Print</Link></td>
-              </tr>
-            ))
-          ) : (
-            <tr>
-              <td colSpan={isPaidTab ? 9 : 11} className="text-center">
-                No records available
-              </td>
+    <Table striped bordered hover>
+      <thead>
+        <tr>
+          <th onClick={() => handleSortChange('ticketNumber')}>Ticket Number</th>
+          <th onClick={() => handleSortChange('licenseNumber')}>License Number</th>
+          <th onClick={() => handleSortChange('dateApprehended')}>Date Apprehended</th>
+          <th onClick={() => handleSortChange('streetApprehended')}>Street Apprehended</th>
+          <th onClick={() => handleSortChange('plateNumber')}>Plate Number</th>
+          <th onClick={() => handleSortChange('vehicleColor')}>Vehicle Color</th>
+          <th onClick={() => handleSortChange('apprehendingOfficer')}>Apprehending Officer</th>
+          <th>Amount/Paid</th>
+          {!isPaidTab && <th onClick={() => handleSortChange('commuteStatus')}>Commuted</th>}
+          {!isPaidTab && <th>Paid</th>}
+          <th>Print</th>
+        </tr>
+      </thead>
+      <tbody>
+        {citations.length > 0 ? (
+          citations.map((citation) => (
+            <tr key={citation._id} className={getRowClass(citation.dateApprehended)}>
+              <td>{citation.ticketNumber}</td>
+              <td>{citation.licenseNumber}</td>
+              <td>{formatDate(citation.dateApprehended)}</td>
+              <td>{citation.streetApprehended}</td>
+              <td>{citation.plateNumber}</td>
+              <td>{citation.vehicleColor}</td>
+              <td>{citation.apprehendingOfficer}</td>
+              <td>{sumAmounts(citation.violations) + " : " + citation.amountPaid}</td>
+              {!isPaidTab && (
+                <td>
+                  {user.userrole === 'dpshead' && (!citation.commuteStatus || citation.commuteStatus === undefined) ? <Button variant="warning" onClick={() => handleCommuteClick(citation)}>Commute</Button> : 'No'}
+                </td>
+              )}
+              {!isPaidTab && (
+                <td>
+                  {user.userrole === 'dpsstaff' && (!citation.paymentStatus || citation.paymentStatus === undefined) ? <Button variant="warning" onClick={() => handlePaymentClick(citation)}>Pay</Button> : 'No'}
+                </td>
+              )}
+              <td><Link>Print</Link></td>
             </tr>
-          )}
-        </tbody>
-      </Table>
-    </>
+          ))
+        ) : (
+          <tr>
+            <td colSpan={isPaidTab ? 9 : 11} className="text-center">
+              No records available
+            </td>
+          </tr>
+        )}
+      </tbody>
+    </Table >
   );
 };
 
